78de9846c86acc575dfd5ea8bbfaf7583a18b6d6
ElenaGeray <116456664+ElenaGeray@users.noreply.github.com>
2022-12-18 16:54:31 +0300

    Add files via upload

---
+++ my_config.py
@@ -0,0 +1,14 @@
+@@ -1,10 +1,10 @@
+user_token = 'vk1.a.sBQrHRkFNb8sQGYdUKt6WBC6TSOHJzUiUNfdQsW-Ia_vGMBs-YCoyRymeA7mLLd2fY4XPr0umU1su6UZJwDIVYsZw0Egf-nZNlpceLDWjllzWkB_6Nz3GpTfaxh2pnvT6xbBaQW7MsgInRcXz82aEAJoK9OrzpFHdv_3UjS31M-oaS8t6QHl3sVFkfqS-zVc' # токен пользователя
+comm_token = 'vk1.a.j9E4AHS63egttHWC2gNSbJG9QubOLx60-tcX_Rr1nKwwieKRG3pDq6ZiSanldOzLdIL00_Mix1uMv2Hw65vMDSpTSqvFIqxOyhBqt7l93CQwkw6T9k-XFhZ1Fm0mV3vIyzSn0cQk1fqjpa_mKGsYqcWc2wj3IrV7cjOF5NC0SnnLU8Z-CcqToOhU7PqLrc6FSSoHlm_JhLvrIY5-VCt9Xg' # токен сообщества
+user_token = '' # токен пользователя
+comm_token = '' # токен сообщества
+
+offset = 0 #сдвиг
+line = range(0, 1000) # последовательность для перебора найденных пользователей
+
+host = '127.0.0.1' 
+user = 'postgres'
+password = 'postgres'
+password = ''
+db_name = 'vk_base'
